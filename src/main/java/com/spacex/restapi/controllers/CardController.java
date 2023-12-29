dbe660fbfe116e776647238e7163fb20aacc7b2c
gonzalo <gonzalohoyos95@gmail.com>
2023-12-29 17:00:57 -0300

    add CardController.java

--- src/main/java/com/spacex/restapi/controllers/CardController.java
+++ src/main/java/com/spacex/restapi/controllers/CardController.java
@@ -1,13 +1,20 @@
 package com.spacex.restapi.controllers;
 
+import com.spacex.restapi.exceptions.TrelloApiException;
 import com.spacex.restapi.models.TrelloCard;
 import com.spacex.restapi.services.TrelloService;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@RestController
+@RequestMapping("/api/trello")
 public class CardController {
     
     private final TrelloService trelloService;
@@ -20,10 +27,18 @@ public class CardController {
     public ResponseEntity<String> createCard(@RequestBody TrelloCard trelloCard) {
         try {
             
-            trelloService.createCard(trelloCard);
-            return ResponseEntity.ok("Card created successfully with");
-        } catch (Exception e) {
+            String cardInfo = trelloService.createCard(trelloCard).block();
+            log.debug("Card created successfully: " + cardInfo);
+            return ResponseEntity.ok("Card created successfully: " + cardInfo);
+            
+        } catch (TrelloApiException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.badRequest().body("Failed to create card: " + e.getMessage());
+            
+        } catch (IllegalArgumentException e) {
+            log.error(e.getMessage());
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
+    
 }
---
+++ src/main/java/com/spacex/restapi/exceptions/TrelloApiException.java
@@ -0,0 +1,6 @@
+package com.spacex.restapi.exceptions;
+
+public class TrelloApiException extends Throwable {
+    public TrelloApiException(String s) {
+    }
+}
